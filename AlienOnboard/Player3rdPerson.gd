02c5c1b6675f3298ce8fd48962b73c1170bd6ac0
tomdamo <20182376@student.hhs.nl>
2024-03-19 13:10:01 +0100

    Dash with timer

--- AlienOnboard/Player3rdPerson.gd
+++ AlienOnboard/Player3rdPerson.gd
@@ -3,9 +3,16 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const DASH_SPEED = 17.0
+const DASH_TIME = 0.2
+
+var dashing = false
+var dash_direction = Vector3.ZERO
+var dash_timer = 0.0
 
 @onready var pivot = $CamOrigin
 @export var sens = 0.5
+@onready var dash_cooldown_timer = $DashCooldownTimer
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -23,19 +30,34 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+
+	# Quit game on ESC, debug purposes.
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+
+	# Space to dash
+	if Input.is_action_just_pressed("dash") and not dashing and dash_cooldown_timer.is_stopped():
+		dash_direction = direction
+		dashing = true
+		dash_timer = DASH_TIME
+		dash_cooldown_timer.start()
+
+	if dashing:
+		velocity = dash_direction * DASH_SPEED
+		dash_timer -= delta
+		if dash_timer <= 0:
+			dashing = false
+
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
