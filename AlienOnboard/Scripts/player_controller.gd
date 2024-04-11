9f64dc81b3fc083e9379ec8ee42e96747eb75196
tomdamo <20182376@student.hhs.nl>
2024-04-11 12:47:58 +0200

    Sign and text in tutorial
    make the area recognizable for the player. Next up, quest system

--- AlienOnboard/Scripts/player_controller.gd
+++ AlienOnboard/Scripts/player_controller.gd
@@ -98,7 +98,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("SenseAbility") and not senseActive and sense_cooldown_timer.is_stopped():
 		print("if for senseability passed")
 		_activateSenseAbility()
-	if Input.is_action_just_pressed("Interact") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 			
 
