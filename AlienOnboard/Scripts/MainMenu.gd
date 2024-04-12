916586c68590470fb0b2051f5b227240dd0c5f56
FuratSchool <36156390+FuratSchool@users.noreply.github.com>
2024-04-12 16:23:25 +0200

    fixed old_main_menu

--- AlienOnboard/Scripts/MainMenu.gd
+++ AlienOnboard/Scripts/MainMenu.gd
@@ -5,15 +5,15 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$VBoxContainer/StartButton.grab_focus();
-	
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if loadTime.is_stopped():
 		get_tree().change_scene_to_file("res://Scenes/Tutorial.tscn");
-	
+
 func _on_start_button_pressed():
 	loadTime.start()
-	
+
 #this needs to change, to get back to the previous scene / menu.
 func _on_options_button_pressed():
 	get_tree().change_scene_to_file("res://UI/OptionsMenu.tscn");
