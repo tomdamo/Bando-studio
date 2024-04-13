84e799721f48f3a0b0c0544fa1b452d64bd3fe74
tomdamo <20182376@student.hhs.nl>
2024-04-13 02:33:28 +0200

    Loading + pause screen added
    also fixed main menu

--- AlienOnboard/Scripts/MainMenu.gd
+++ AlienOnboard/Scripts/MainMenu.gd
@@ -10,8 +10,8 @@ func _process(delta):
 	pass
 	
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Tutorial.tscn");
-	
+	var loadingScreen = load("res://Scenes/UI/loading.tscn")
+	get_tree().change_scene_to_packed(loadingScreen)
 	
 #this needs to change, to get back to the previous scene / menu.
 func _on_options_button_pressed():
