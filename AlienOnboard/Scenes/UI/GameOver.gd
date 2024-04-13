b9f2cb1e0fe591110a217fb67bbaaf835bb17412
tomdamo <20182376@student.hhs.nl>
2024-04-13 03:29:06 +0200

    Game over and pause menu adjustments
    googoo gaagaa i go sleep its 3:28

--- AlienOnboard/Scenes/UI/GameOver.gd
+++ AlienOnboard/Scenes/UI/GameOver.gd
@@ -6,9 +6,9 @@ func _on_resume_pressed():
 	player._pauseMenu()
 
 
-func _on_options_pressed():
-	get_tree().change_scene_to_file("res://UI/OptionsMenu.tscn");
-
-
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_startover_pressed():
+	var _reload = get_tree().reload_current_scene() 
