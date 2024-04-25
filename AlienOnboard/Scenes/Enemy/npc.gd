5ea8b3fb515271fc7b427717a4c26ec341109e81
tomdamo <20182376@student.hhs.nl>
2024-04-25 15:02:54 +0200

    you bloody

--- AlienOnboard/Scenes/Enemy/npc.gd
+++ AlienOnboard/Scenes/Enemy/npc.gd
@@ -4,7 +4,13 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-var health = 3
+var health = 9
+@onready var blood = %BloodEffect
+@onready var bloodTimer = %BloodTimer
+
+func _ready():
+	pass
+
 
 func _physics_process(delta):
 	#TODO make the npc runaway if they see you?
@@ -13,9 +19,18 @@ func _physics_process(delta):
 func take_damage(damage):
 	print("Damage taken")
 	print(damage)	
+	bloodTimer.start()
+	blood.show() 
 	health -= damage
 	if health <= 0:
 		die()
 
 func die():
-	queue_free()
+	print("im ded")
+	self.rotation_degrees.x = 90
+	self.position.y -=1
+	pass
+
+
+func _on_blood_timer_timeout():
+	blood.hide()
