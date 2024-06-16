bfd7d6beecd3464e365eb0f8a0e0d9f2a5562aa4
tomdamo <20182376@student.hhs.nl>
2024-06-16 18:00:54 +0200

    alles behalve de scene :(

---
+++ AlienOnboard/Scenes/ShipFinal.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+@onready var animation_player = $Rooms/Labroom/LabroomCutscene/AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animation_player.play("LabCutscene")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if !animation_player.is_playing():
+		pass
+		 
--- AlienOnboard/Scenes/Ship_level_1_SCRIPT.gd
+++ AlienOnboard/Scenes/Ship_level_1_SCRIPT.gd
@@ -3,10 +3,14 @@ extends Node3D
 @onready var waypoint_1 = $WAYPOINT1
 @onready var player = $Player/PlayerCharacterBody3D
 @onready var waypoint_2 = $WAYPOINT2
+@onready var waypoint_3 = $WAYPOINT3
+@onready var gas_timer = $GasTimer
+@onready var gas_area_3d = $GasArea3D
+@onready var waypoint_4 = $WAYPOINT4
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	gas_timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,6 +27,38 @@ func _on_waypoint_1_body_entered(body):
 
 func _on_waypoint_2_body_entered(body):
 	if "Player" in body.name:
-		waypoint_1.hide()
+		waypoint_2.hide()
 		#Set respawn point/checkpoint
 		player.respawn_point = waypoint_2.get_position()
+
+
+func _on_waypoint_first_body_entered(body):
+	if "Player" in body.name:
+		waypoint_3.hide()
+		#Set respawn point/checkpoint
+		player.respawn_point = waypoint_3.get_position()
+		$Control/Quest1.hide()
+		$Control/Quest2.show()
+		
+func _on_gas_timer_timeout():
+	var overlaps = gas_area_3d.get_overlapping_bodies()
+	for body in overlaps:
+		if body.is_in_group("Player"):
+			player.die()
+			gas_timer.start()
+
+
+func _on_gas_area_3d_body_entered(body):
+	if "Player" in body.name:
+		if gas_timer.is_stopped():
+			player.die()
+			gas_timer.start()
+		else:
+			print("Get yo ass outta the gas")
+
+
+func _on_waypoint_start_body_entered(body):
+	if "Player" in body.name:
+		waypoint_4.hide()
+		#Set respawn point/checkpoint
+		player.respawn_point = waypoint_4.get_position()
