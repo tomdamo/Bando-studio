ba298e597121bec80d062cbb01d161147f80556f
tomdamo <20182376@student.hhs.nl>
2024-06-16 20:10:14 +0200

    finished quest system and finish

--- AlienOnboard/Scenes/Ship_level_1_SCRIPT.gd
+++ AlienOnboard/Scenes/Ship_level_1_SCRIPT.gd
@@ -1,14 +1,28 @@
 extends Node3D
 
 @onready var waypoint_1 = $WAYPOINT1
-@onready var player = $Player/PlayerCharacterBody3D
 @onready var waypoint_2 = $WAYPOINT2
 @onready var waypoint_3 = $WAYPOINT3
 @onready var gas_timer = $GasTimer
 @onready var gas_area_3d = $GasArea3D
 @onready var waypoint_4 = $WAYPOINT4
+@onready var waypointfinal = $WAYPOINTFINAL
 
-# Called when the node enters the scene tree for the first time.
+#quests
+@onready var quest_1 = $Control/Quest1
+@onready var quest_2 = $Control/Quest2
+@onready var quest_3 = $Control/Quest3
+@onready var quest_4 = $Control/Quest4
+@onready var quest_5 = $Control/Quest5
+@onready var quest_6 = $Control/Quest6 
+
+@onready var player = $Player/PlayerCharacterBody3D
+
+
+@onready var security_door = $Rooms/sleep/RootNode/SECURITY_DOOR
+var security_door_open = false
+
+
 func _ready():
 	gas_timer.start()
 
@@ -18,19 +32,27 @@ func _process(delta):
 	pass
 
 
+func open_security():
+	#if alle guards dood zijn
+	security_door.queue_free()
+	pass
+	
+	
 func _on_waypoint_1_body_entered(body):
 	if "Player" in body.name:
 		waypoint_1.hide()
 		#Set respawn point/checkpoint
 		player.respawn_point = waypoint_1.get_position()
-
+		quest_2.hide()
+		quest_3.show()
 
 func _on_waypoint_2_body_entered(body):
 	if "Player" in body.name:
 		waypoint_2.hide()
 		#Set respawn point/checkpoint
 		player.respawn_point = waypoint_2.get_position()
-
+		quest_4.hide()
+		quest_5.show()
 
 func _on_waypoint_first_body_entered(body):
 	if "Player" in body.name:
@@ -62,3 +84,9 @@ func _on_waypoint_start_body_entered(body):
 		waypoint_4.hide()
 		#Set respawn point/checkpoint
 		player.respawn_point = waypoint_4.get_position()
+
+
+func _on_waypoint_final_body_entered(body):
+	if "Player" in body.name:
+		waypointfinal.hide()
+		get_tree().change_scene_to_file("res://END.tscn")
